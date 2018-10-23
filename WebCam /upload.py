3de9787128f8ae08d889a55130e52ae935289f08
Ian Franco <ianfranco.1986@gmail.com>
2018-10-23 07:48:45 -0300

    s
    s

---
+++ WebCam /upload.py
@@ -0,0 +1,51 @@
+import time
+import cv2
+import os 
+import paramiko 
+
+
+#Identificación de la camara
+camera_id = 1
+
+camera = cv2.VideoCapture(0)
+
+#Conexión
+host = "areatecnica.cl"
+port = 22222
+user = "root"
+password = "NintendO64"
+server_path = '/var/www/html/images/'
+local_path = 'home/image.jpg'
+
+
+#Tamaño de la captura
+height=480*3
+width=640*3
+
+
+def upload():
+	transport = paramiko.Transport((host, port))
+	transport.connect(username=user, password = password)
+	sftp = paramiko.SFTPClient.from_transport(transport)
+	sftp.put('image.jpg', server_path+str(camera_id)+'.jpg')
+	
+	sftp.close()
+	transport.close()
+	return 'Imagen subida exitosamente'
+
+try:
+	os.remove("image.jpg")
+	print ("Limpiando datos previos")
+except Exception:
+	print ("No se han encontrados datos previos")
+
+while True:
+	
+	ret, frame = camera.read()
+	cv2.imwrite('image.jpg', frame)
+
+	print(upload()+" "+time.strftime("%Y-%m-%d %H:%M:%S"))
+
+	time.sleep(2)
+
+camera.release();
